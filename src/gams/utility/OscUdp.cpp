4c1f94f2155d8e016c9b6da9f9d781c2663b771a
racsoraul <racsoraul@gmail.com>
2019-02-22 20:58:54 -0600

    Added OSCPacketListener to implement the receive function. Removed unpack and create_socket functions.

--- src/gams/utility/OscUdp.cpp
+++ src/gams/utility/OscUdp.cpp
@@ -15,6 +15,49 @@
 #include "oscpack/osc/OscOutboundPacketStream.h"
 #include "oscpack/ip/UdpSocket.h"
 
+#include "osc/OscReceivedElements.h"
+#include "osc/OscPacketListener.h"
+#include "ip/UdpSocket.h"
+
+class OSCPacketListener : public osc::OscPacketListener
+{
+  protected:
+    virtual void ProcessMessage(const osc::ReceivedMessage &m,
+                                const IpEndpointName &remoteEndpoint)
+    {
+        (void)remoteEndpoint; // suppress unused parameter warning
+
+        try
+        {
+            if (std::strcmp(m.AddressPattern(), "/agent/0/pos") == 0)
+            {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                float x, y, z;
+                args >> x >> y >> z >> osc::EndMessage;
+
+                std::cout << "pos from /agent/0: "
+                             "x = " << x << " y = " << y << " z = " << z << "\n";
+            }
+            else if (std::strcmp(m.AddressPattern(), "/agent/0/rot") == 0)
+            {
+                osc::ReceivedMessageArgumentStream args = m.ArgumentStream();
+                float x, y, z;
+                args >> x >> y >> z >> osc::EndMessage;
+
+                std::cout << "rot from /agent/0: "
+                             "x = " << x << " y = " << y << " z = " << z << "\n";
+            }
+        }
+        catch (osc::Exception &e)
+        {
+            // any parsing errors such as unexpected argument types, or
+            // missing arguments get thrown as exceptions.
+            std::cout << "error while parsing message: "
+                      << m.AddressPattern() << ": " << e.what() << "\n";
+        }
+    }
+};
+
 size_t
 gams::utility::OscUdp::pack (osc::OutboundPacketStream bundle, size_t output_buffer_size, const OscMap& map)
 {
@@ -27,7 +70,7 @@ gams::utility::OscUdp::pack (osc::OutboundPacketStream bundle, size_t output_buf
         madara::utility::ends_with (i.first, "/velocity/z") ||
         madara::utility::ends_with (i.first, "/yaw")
       ) {
-      bundle << osc::BeginMessage(i.first.c_str)
+      bundle << osc::BeginMessage(i.first.c_str())
         << i.second[0] << i.second[1]
         << osc::EndMessage;
     }
@@ -38,51 +81,22 @@ gams::utility::OscUdp::pack (osc::OutboundPacketStream bundle, size_t output_buf
   return bundle.Size();
 }
 
-
-void
-gams::utility::OscUdp::unpack (char* buffer, size_t size, OscMap & map)
-{
-  // @Alex Rozgo. I've removed all of this and changed the function signature
-  // to be more generic
-}
-
 int
-gams::utility::OscUdp::receive (OscMap & values, double max_wait_seconds)
+gams::utility::OscUdp::receive (OscMap & dest, double max_wait_seconds)
 {
-  int result = -1;
-  boost::asio::ip::udp::endpoint remote;
-  size_t bytes_read;
+  gams::utility::OscUdp::OscMap source_map;
+  
+  //TODO: insert data into the `dest` map.
+  OSCPacketListener listener;
+    UdpListeningReceiveSocket socket(
+        IpEndpointName(IpEndpointName::ANY_ADDRESS, 8000),
+        &listener);
+  
+  std::cout << "press ctrl-c to end\n";
 
-  if (has_socket())
-  {
-    madara::utility::EpochEnforcer<madara::utility::Clock> enforcer(
-      0.0, max_wait_seconds);
+  socket.RunUntilSigInt();
 
-    do
-    {
-      result = transport_.get()->receive_buffer(
-        buffer_.get(), bytes_read, remote);
-
-      if (result == 0 && bytes_read != 0)
-      {
-        madara_logger_ptr_log(gams::loggers::global_logger.get(),
-          gams::loggers::LOG_MAJOR,
-          "gams::utility::OscUdp::receive: " \
-          " received %zu bytes. Calling unpack\n",
-          bytes_read);
-
-        unpack(buffer_.get(), bytes_read, values);
-      }
-    } while (result == 0 && !enforcer.is_done());
-  }
-
-  madara_logger_ptr_log(gams::loggers::global_logger.get(),
-    gams::loggers::LOG_MAJOR,
-    "gams::utility::OscUdp::receive: " \
-    " returning result %d with %zu values\n",
-    result, values.size());
-
-  return result;
+  return 0;
 }
 
 #define ADDRESS "127.0.0.1"
@@ -96,7 +110,7 @@ gams::utility::OscUdp::send (const OscMap & values)
 
   char buffer[OUTPUT_BUFFER_SIZE];
   osc::OutboundPacketStream bundle(buffer, OUTPUT_BUFFER_SIZE);
-  
+
   pack(bundle, OUTPUT_BUFFER_SIZE, values);
 
   transmitSocket.Send(bundle.Data(), bundle.Size());
